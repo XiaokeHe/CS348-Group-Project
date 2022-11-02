c19d0437cceb2c34e3fb519840ef368407f5f9d3
Xiaoke He <70498251+XiaokeHe@users.noreply.github.com>
2022-11-02 10:44:36 -0400

    Update Run.java

--- GUI/src/Run.java
+++ GUI/src/Run.java
@@ -1,14 +1,20 @@
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.sql.SQLException;
 
 public class Run {
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
+        String jdbc = "jdbc:mysql://localhost:3306/Library Managament";
+        String username = "root";
+        String password = "20020530hxk!";
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new Frame("Share File!!");
+            JFrame frame = null;
+            try {
+                frame = new Frame("Library Management System", jdbc, username, password);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
             frame.setVisible(true);
         });
-
-
     }
 }
