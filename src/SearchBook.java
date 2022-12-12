4c93d66ea83d6f71a34bfc44df28e4493feb947f
Xiaoke He <70498251+XiaokeHe@users.noreply.github.com>
2022-12-11 21:08:37 -0500

    Add files via upload
    Kristy 12/11 Search books and show the search result

---
+++ src/SearchBook.java
@@ -0,0 +1,176 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.math.BigInteger;
+import java.sql.*;
+
+public class SearchBook extends JComponent implements Runnable {
+    static JFrame searchFrame;
+    static JTextField searchInfo;
+    static JButton searchButton;
+    static JComboBox<String> dropdown;
+    private static String info;
+    private static String action;
+    private static String filter;
+    Statement statement;
+    public SearchBook(Statement statement) {
+        info = null;
+        action = null;
+        this.statement =statement;
+    }
+
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == searchButton) {
+                action = "search";
+                filter = dropdown.getSelectedItem().toString();
+                System.out.println(filter);
+                if (searchInfo.getText().equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please enter the book information!",
+                            "Missing Information", JOptionPane.ERROR_MESSAGE);
+                }
+                else {
+                    ResultSet r1 = null;
+                    info = searchInfo.getText();
+                    searchInfo.setText("");
+                    if (filter.equals("Book ID")) {
+                        if ((!info.substring(0,1).equals("B")) && (!info.substring(0, 1).equals("b"))) {
+                            info = "B" + info;
+                        }
+                        if (info.substring(0,1).equals("b")) {  // sometimes users will use lowercase
+                            info = "B" + info.substring(1);
+                        }
+                        String sql = "SELECT Book.book_id AS Book_ID, Book_Info.ISBN AS ISBN, Book_Info.title AS Title, Book_Info.author AS Author, " +
+                                "Book_Info.genre AS Genre, Book_Info.price AS Price, Book.location_id AS Location, Book.status AS Status " +
+                                "FROM Book_Info JOIN Book ON Book.ISBN = Book_Info.ISBN " +
+                                "WHERE Book.book_id = '" + info + "'";
+                        try {
+                            r1 = statement.executeQuery(sql);
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                    else if (filter.equals("ISBN")) {
+                        String sql = "SELECT Book.book_id, Book_Info.ISBN, Book_Info.title, Book_Info.author, " +
+                                "Book_Info.genre, Book_Info.price, Book.location_id, Book.status " +
+                                "FROM Book_Info JOIN Book ON Book.ISBN = Book_Info.ISBN " +
+                                "WHERE Book.ISBN = '" + info + "'";
+                        try {
+                            r1 = statement.executeQuery(sql);
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                    else if (filter.equals("Title")) {
+                        String[] titles = info.split(" ");
+                        StringBuilder newInfo = new StringBuilder();
+                        for(String t : titles) {
+                            t = t.toLowerCase();
+                            String first = t.substring(0,1).toUpperCase();
+                            newInfo.append(" ").append(first).append(t.substring(1));
+                        }
+                        String sql = "SELECT Book.book_id AS Book_ID, Book_Info.ISBN AS ISBN, Book_Info.title AS Title, Book_Info.author AS Author, " +
+                                "Book_Info.genre AS Genre, Book_Info.price AS Price, Book.location_id AS Location, Book.status AS Status " +
+                                "FROM Book_Info JOIN Book ON Book.ISBN = Book_Info.ISBN " +
+                                "WHERE Book_Info.title LIKE '%" + newInfo.substring(1) + "%'";
+                        try {
+                            r1 = statement.executeQuery(sql);
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                    else if (filter.equals("Author")) {
+                        String[] names = info.split(" ");
+                        StringBuilder name = new StringBuilder();
+                        for(String n : names) {
+                            n = n.toLowerCase();
+                            String first = n.substring(0,1).toUpperCase();
+                            name.append(" ").append(first).append(n.substring(1));
+                        }
+                        String sql = "SELECT Book.book_id AS Book_ID, Book_Info.ISBN AS ISBN, Book_Info.title AS Title, Book_Info.author AS Author, " +
+                                "Book_Info.genre AS Genre, Book_Info.price AS Price, Book.location_id AS Location, Book.status AS Status " +
+                                "FROM Book_Info JOIN Book ON Book.ISBN = Book_Info.ISBN " +
+                                "WHERE Book_Info.author LIKE '%" +name.substring(1) + "%'";
+                        try {
+                            r1 = statement.executeQuery(sql);
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                    }
+                    else {
+                        String[] genres = info.split(" ");
+                        StringBuilder genre = new StringBuilder();
+                        for(String g: genres) {
+                            g = g.toLowerCase();
+                            String first = g.substring(0, 1).toUpperCase();
+                            genre.append(" ").append(first).append(g.substring(1));
+                        }
+                        String sql = "SELECT Book.book_id AS Book_ID, Book_Info.ISBN AS ISBN, Book_Info.title AS Title, Book_Info.author AS Author, " +
+                                "Book_Info.genre AS Genre, Book_Info.price AS Price, Book.location_id AS Location, Book.status AS Status " +
+                                "FROM Book_Info JOIN Book ON Book.ISBN = Book_Info.ISBN " +
+                                "WHERE Book_Info.genre LIKE '%" + genre.substring(1)  + "%'";
+                        try {
+                            r1 = statement.executeQuery(sql);
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                    ShowBookSearchResult result = new ShowBookSearchResult(filter, info, r1);
+                    SwingUtilities.invokeLater(result);
+                }
+            }
+        }
+    };
+
+    WindowListener windowListener = new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            action = "close";
+        }
+    };
+
+    public String getInfo() {
+        return info;
+    }
+
+    @Override
+    public void run() {
+        //Frame
+        searchFrame = new JFrame("Search Books");
+        Container content = searchFrame.getContentPane();
+        content.setLayout(new BorderLayout());
+        searchFrame.setSize(600, 200);
+        searchFrame.setLocationRelativeTo(null);
+        searchFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        searchFrame.addWindowListener(windowListener);
+
+        //Dropdown Menu
+        JLabel select = new JLabel("Search by:");
+        select.setVisible(true);
+        String[] choices = {"Book ID", "ISBN", "Title", "Author", "Genre"};
+        dropdown = new JComboBox<String>(choices);
+        dropdown.setVisible(true);
+        JPanel panel1 = new JPanel();
+        panel1.add(select);
+        panel1.add(dropdown);
+
+        //Search Information
+        JLabel labelInfo = new JLabel("Enter the information of the book:");
+        searchInfo = new JTextField(15);
+        searchInfo.addActionListener(actionListener);
+        searchButton = new JButton("Search");
+        searchButton.addActionListener(actionListener);
+        JPanel panel2 = new JPanel();
+        panel2.add(labelInfo);
+        panel2.add(searchInfo);
+        panel2.add(searchButton);
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(new BoxLayout(panel3, BoxLayout.PAGE_AXIS));
+        panel3.add(panel1);
+        panel3.add(panel2);
+        content.add(panel3, BorderLayout.NORTH);
+        searchFrame.setVisible(true);
+    }
+}
---
+++ src/ShowBookSearchResult.java
@@ -0,0 +1,85 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.math.BigInteger;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ShowBookSearchResult extends JComponent implements Runnable {
+    static JFrame resultFrame;
+    private String action;
+    //Information
+    String filter;
+    String bookInfo;
+    ResultSet r;
+
+    public ShowBookSearchResult(String filter, String bookInfo, ResultSet r){
+        this.filter = filter;
+        this.bookInfo = bookInfo;
+        this.r = r;
+        action = null;
+    }
+
+    WindowListener windowListener = new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            action = "close";
+        }
+    };
+
+    @Override
+    public void run() {
+        //Design Frame
+        resultFrame = new JFrame("Search Results");
+        Container content = resultFrame.getContentPane();
+        content.setLayout(new BorderLayout());
+        resultFrame.setSize(850, 250);
+        resultFrame.setLocationRelativeTo(null);
+        resultFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        resultFrame.addWindowListener(windowListener);
+
+        // "Search by:<filter>:<bookInfo>(# of books)"
+        JPanel panel1 = new JPanel();
+
+        //Get books
+        ArrayList<String[]> books = new ArrayList<>();
+        String[] attributes = {"Book_ID", "ISBN", "Title", "Author", "Genre", "Price", "Location", "Status"};
+        try {
+            while(r.next()) {
+                String[] b = new String[attributes.length];
+                for(int i = 0; i < attributes.length; i++) {
+                    b[i] = r.getString(attributes[i]);
+                }
+                books.add(b);
+            }
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        String[][] data = new String[books.size()][attributes.length];
+        for(int i = 0; i < books.size(); i++) {
+            data[i] = books.get(i);
+        }
+        JTable table = new JTable(data, attributes);
+        table.setVisible(true);
+        table.getColumnModel().getColumn(1).setPreferredWidth(110);
+        table.getColumnModel().getColumn(2).setPreferredWidth(140);
+        table.getColumnModel().getColumn(3).setPreferredWidth(140);
+        table.getColumnModel().getColumn(4).setPreferredWidth(170);
+        table.getColumnModel().getColumn(5).setPreferredWidth(50);
+        table.getColumnModel().getColumn(6).setPreferredWidth(90);
+        table.getColumnModel().getColumn(7).setPreferredWidth(70);
+        String s = "Search by: " + filter + " : " + bookInfo + " (" + data.length + " book(s) are found)";
+        JLabel summary = new JLabel(s);
+        panel1.add(summary);
+        JPanel panel2 = new JPanel();
+        panel2.add(table.getTableHeader(), BorderLayout.NORTH);
+        panel2.add(table);
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(new BoxLayout(panel3, BoxLayout.PAGE_AXIS));
+        panel3.add(panel1);
+        panel3.add(panel2);
+        content.add(panel3, BorderLayout.CENTER);
+        resultFrame.setVisible(true);
+        }
+}
