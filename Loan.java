ad75731e119cbbd2f32a0f2f98562dcbb22455e1
jiakun421 <60355161+jiakun421@users.noreply.github.com>
2022-12-12 15:54:17 -0500

    Update Loan.java

--- Loan.java
+++ Loan.java
@@ -12,14 +12,11 @@ import java.sql.ResultSet;
         static BigInteger isbn;
         static JButton add;
         static String book_id;
-        static String c_id;
-        static String r_id;
-        static String Isbn;
+
         private static String action;
-        static JTextArea isbnField;
+
         static JTextArea bookIdField;
-        static JTextArea RIDField;
-        static JTextArea CIDField;
+
         Statement statement;
 
         ActionListener actionListener = new ActionListener() {
@@ -27,31 +24,17 @@ import java.sql.ResultSet;
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource() == add) {
                     action = "add";
-                    if (!isbnField.getText().equals("") && !bookIdField.getText().equals("") && !RIDField.getText().equals("") && !CIDField.getText().equals("")) {
-                        isbn = new BigInteger(isbnField.getText());
-                        BigInteger isbnMin = new BigInteger("1000000000000");
-                        BigInteger isbnMax = new BigInteger("9999999999999");
-                        if (isbn.compareTo(isbnMin) < 0 || isbn.compareTo(isbnMax) > 0) {
-                            JOptionPane.showMessageDialog(null, "ISBN has to be 13 digits number",
-                                    "Book Info Error", JOptionPane.ERROR_MESSAGE);
-                            action = "no";
-                        }
+                    if (!bookIdField.getText().equals("") ) {
                         book_id = bookIdField.getText();
-                        c_id = CIDField.getText();
-                        r_id = RIDField.getText();
-                        isbnField.setText("");
                         bookIdField.setText("");
-                        CIDField.setText("");
-                        RIDField.setText("");
                     } else {
                         JOptionPane.showMessageDialog(null, "Please enter all information",
                                 "Book Info Error", JOptionPane.ERROR_MESSAGE);
                         action = "no";
                     }
-                    System.out.println(isbn);
                     if (action != "no") {
                         try {
-                            String sql = "INSERT INTO Loan_Record VALUES(' " + r_id +", '"+ book_id + "','" + c_id + "','" + isbn + ")";
+                            String sql = "INSERT INTO Loan_Record VALUES('"+ book_id + "')";
                             int x = statement.executeUpdate(sql);
                         } catch (Exception e1) {
                             e1.printStackTrace();
@@ -73,7 +56,7 @@ import java.sql.ResultSet;
         {
             super(title);
             setLayout(new BorderLayout());
-            this.setSize(1000, 200);
+            this.setSize(500, 200);
             Toolkit computer1 = Toolkit.getDefaultToolkit();
             Dimension dim = computer1.getScreenSize();
             int x = (dim.width / 2) - (this.getWidth() / 2);
@@ -86,9 +69,7 @@ import java.sql.ResultSet;
 
 
             JPanel parent = new JPanel();
-            Box R_id = Box.createHorizontalBox();
-            Box ISbn = Box.createHorizontalBox();
-            Box C_id = Box.createHorizontalBox();
+
             Box Book_id = Box.createHorizontalBox();
 
 
@@ -96,12 +77,7 @@ import java.sql.ResultSet;
             JPanel receive_box = new JPanel();
             JPanel adjust = new JPanel();
 
-            JLabel isbnLabel = new JLabel("Enter the ISBN:");
-            isbnField = new JTextArea();
-            isbnField.setColumns(10);
-            isbnField.setRows(1);
-            ISbn.add(isbnLabel);
-            ISbn.add(isbnField);
+
 
             JLabel book_id = new JLabel("Enter the book_id:");
             bookIdField = new JTextArea();
@@ -110,19 +86,7 @@ import java.sql.ResultSet;
             Book_id.add(book_id);
             Book_id.add(bookIdField);
 
-            JLabel r_label = new JLabel("Enter the record_id:");
-            RIDField = new JTextArea();
-            RIDField.setColumns(10);
-            RIDField.setRows(1);
-            R_id.add(r_label);
-            R_id.add(RIDField);
 
-            JLabel C_label = new JLabel("Enter the customer_id:");
-            CIDField = new JTextArea();
-            CIDField.setColumns(10);
-            CIDField.setRows(1);
-            C_id.add(C_label);
-            C_id.add(CIDField);
 
 
             JTextArea received_view = new JTextArea();
@@ -134,15 +98,13 @@ import java.sql.ResultSet;
             adjust.add(received_view);
             adjust.add(scroll_button);
 
-            parent.add(ISbn);
-            parent.add(C_id);
+
             parent.add(Book_id);
 
-            parent.add(R_id);
+
             parent.add(receive_box);
             parent.add(adjust);
             this.add(parent);
             this.statement = statement;
         }
     }
-
